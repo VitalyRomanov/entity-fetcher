9b72eef6ec71bdc916464f83625142dfdf5bee86
Vitaly R <mortiv16@gmail.com>
2019-09-15 16:29:03 +0300

    improved tokenizer pattern

--- LanguageTools.jl
+++ LanguageTools.jl
@@ -1,5 +1,7 @@
 module LanguageTools
 
+diacritics = "\u0300-\u036F"
+
 function load(filepath)
     lemma_dict_file = open(filepath, "r")
     lemma_dict = Dict()
@@ -10,8 +12,11 @@ function load(filepath)
     return lemma_dict
 end
 
+
 function tokenize(line)
-    matches = eachmatch(r"[A-Za-zА-Яа-я][A-Za-zА-Яа-я-]+|[A-Za-zА-Яа-я]|[^\w\s]|[0-9]+", line)
+    # cyrillic = "\u0400-\u04FF\u0500-\u052F\u2DE0-\u2DFF\uA640-\uA69F\u1C80-\u1C8F\u1D2B\u1D78\uFE2E\uFE2F"
+    re = Regex("[\\w$diacritics][\\w$diacritics-]+[\\w$diacritics]|[\\w$diacritics]+|[^\\w\\s]")
+    matches = eachmatch(re, line)
     tokens = [m.match for m in matches]
     return tokens
 end
@@ -22,3 +27,4 @@ end
 # println(en_lemm["has"])
 
 # println(Lemmatizer.tokenize("a bb cbb"))
+# println(LanguageTools.tokenize("Манхэ́ттен[1] (англ. Manhattan [mænˈhætən]) — историческое ядро города Нью-Йорка и одно из его пяти боро. Кроме острова Манхэттен, боро включает в себя несколько небольших островов (см. География Манхэттена). "))
